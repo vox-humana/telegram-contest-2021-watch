a9087da08fc222ac67da2e2c775592cd95cfba9a
Arthur Semenyutin <semenyutin@gmail.com>
2021-03-06 22:23:30 +1100

    Initial Commit

---
+++ TGWatchOS WatchKit Extension/TGWatchOSApp.swift
@@ -0,0 +1,19 @@
+//
+//  TGWatchOSApp.swift
+//  TGWatchOS WatchKit Extension
+//
+//  Created by Arthur Semenyutin on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TGWatchOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
